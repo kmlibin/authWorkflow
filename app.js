887e24562e97d084f59e2f27841df33574abc2a2
Kelli Libin <km.putnam2@gmail.com>
2023-06-14 14:17:12 -0600

    set up server

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('E-Commerce API');
+//dotenv
+require('dotenv').config()
+
+//server
+const express = require('express');
+const app = express();
+
+//database
+const connectDB = require('./db/connect')
+
+//set up port variable and start function
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log('server is listening on 5000...'))
+    }catch(err) {
+        console.log(err)
+    }
+}
+
+start();
