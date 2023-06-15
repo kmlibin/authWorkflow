da9e56860cbdec4db8086a32f54ade0b7eb82efd
Kelli Libin <km.putnam2@gmail.com>
2023-06-15 13:31:57 -0600

    guard so that frontend can't manipulate role

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,14 +5,16 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 
 const registerUser = async (req, res) => {
+  
+  const { email, name, password } = req.body;
+  
   //check for duplicate email
-  const { email } = req.body;
   const checkIfExists = await User.findOne({ email });
   if (checkIfExists) {
     throw new CustomError.BadRequestError("email already in use");
   }
   //create a new user
-  const user = await User.create(req.body);
+  const user = await User.create({name, email, password});
   res.status(StatusCodes.CREATED).json({
     user,
   });
