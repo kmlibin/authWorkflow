3c432ed67a97350f55ff61b779e0e7be2eaaa09a
Kelli Libin <km.putnam2@gmail.com>
2023-06-15 11:13:23 -0600

    check for duplicate email and create user in reg

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,21 @@
 //import model
-// const User = require("../models/User");
+const User = require("../models/User");
+//status codes & errors
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
 
 const registerUser = async (req, res) => {
-  res.send("register user");
+  //check for duplicate email
+  const { email } = req.body;
+  const checkIfExists = await User.findOne({ email });
+  if (checkIfExists) {
+    throw new CustomError.BadRequestError("email already in use");
+  }
+  //create a new user
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({
+    user,
+  });
 };
 
 const loginUser = async (req, res) => {
@@ -13,5 +26,4 @@ const logoutUser = async (req, res) => {
   res.send("logout user");
 };
 
-
-module.exports = {registerUser, loginUser, logoutUser};
+module.exports = { registerUser, loginUser, logoutUser };
