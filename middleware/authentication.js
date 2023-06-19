bbc19f70decf3a2cf7f5ec177b2eae6c12451dfe
Kelli Libin <km.putnam2@gmail.com>
2023-06-19 16:55:39 -0600

    finished auth user, added comments about flow

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,14 +6,23 @@ const { isTokenValid } = require("../utils");
 const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
   if (!token) {
-    console.log("no token");
-  } else {
-    console.log("token present");
+    throw new CustomError.UnauthenticatedError("Authentication Invalid");
   }
-
-  next();
+  try {
+    //when user lgs in, creates a token user, attaches cookies to response, createJWT and user is payload. user info is encoded
+    //into the token. Istokenvalid uses jwt.verify, which decodes the token and thus the user. we then add it to the req object
+    const payload = isTokenValid({ token }); 
+    
+    //adding to the request object
+    req.user = { name: payload.name, userId: payload._id, role: payload.role };
+    //payload shows user object..why? payload in isTokenValid does not return this
+    // console.log(payload);
+    next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError("Authentication Invalid");
+  }
 };
 
-module.exports = {authenticateUser};
+module.exports = { authenticateUser };
 
 //all user routes will need this, so can go to app.js and stick it in front of user router. or, set it up in your routes.
