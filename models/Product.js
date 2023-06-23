194af3b5fc8001c1aa93253b8f0853b90a6aa96e
Kelli Libin <km.putnam2@gmail.com>
2023-06-23 11:19:45 -0600

    create product model

---
+++ models/Product.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "please provide product name"],
+      maxlength: [100, "cannot be more than 100 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "please provide product price"],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, "please provide product description"],
+      maxlength: [1000, "cannot be more than 1000 characters"],
+    },
+    image: {
+      type: String,
+      default: "/uploads/example.jpeg",
+    },
+    category: {
+      type: String,
+      required: [true, "please provide category"],
+      enum: ["office", "kitchen", "bedroom"],
+    },
+    company: {
+      type: String,
+      required: [true, "please provide a company"],
+      //another way to set up enum
+      enum: {
+        values: ["ikea", "liddy", "marcos"],
+        message: "{VALUE} is not supported",
+      },
+    },
+    colors: {
+      type: [String],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      required: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      required: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  //gives fields createdat and updated at
+  { timeStamps: true }
+);
+
+module.exports = mongoose.Model("Product", ProductSchema);
