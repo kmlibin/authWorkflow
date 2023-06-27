89d38fc8cd34be2c07ea7b06c099050245292ec5
Kelli Libin <km.putnam2@gmail.com>
2023-06-27 10:53:11 -0600

    implement review router

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 //rest of the packages
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 
 //database
 const connectDB = require("./db/connect");
@@ -20,6 +20,7 @@ const connectDB = require("./db/connect");
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const productRouter = require("./routes/productRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
 
 //middleware
 const notFoundMiddleware = require("./middleware/not-found");
@@ -30,8 +31,8 @@ app.use(express.json());
 //signing our cookies, now available in req.signedCookies
 app.use(cookieParser(process.env.JWT_SECRET));
 //make public folder available (setting it up as our static assets);
-app.use(express.static('./public'))
-app.use(fileUpload())
+app.use(express.static("./public"));
+app.use(fileUpload());
 //home route
 app.get("/", (req, res) => {
   res.send("ecommerce api");
@@ -41,7 +42,7 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
-
+app.use("/api/v1/reviews", reviewRouter);
 //why 404 before errorhandler? error handler needs to come last as per express rules.
 //you only get to it if the route exists and there's an issue
 app.use(notFoundMiddleware);
