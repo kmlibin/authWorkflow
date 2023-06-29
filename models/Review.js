c3f081aa9688ef445c41b5052d14e3d8b8822512
Kelli Libin <km.putnam2@gmail.com>
2023-06-29 11:51:34 -0600

    added calc average rating, post hooks to invoke it

--- models/Review.js
+++ models/Review.js
@@ -34,4 +34,41 @@ const ReviewSchema = new mongoose.Schema(
 //makes so user can only post one review per product
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
+//methods v statics - methods you can call on the instance of user. statics, you call on the schema
+//gotta call it from the below hook
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        //or null in this case, see notes.
+        _id: "$product",
+        averageRating: { $avg: "$rating" },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+  //update - first is the thing to update, second two are props to updatea
+  //optional chaining with result because it can be an empty array
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productId },
+      { averageRating: Math.ceil(result[0]?.averageRating || 0),
+       numOfReviews: result[0]?.numOfReviews || 0 }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//post, will do after the save() is triggered
+ReviewSchema.post("save", async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
+//will do after .remove() is triggered
+ReviewSchema.post("remove", async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
 module.exports = mongoose.model("Review", ReviewSchema);
