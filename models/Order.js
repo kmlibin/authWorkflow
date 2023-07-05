537483bd6ce0224275e5513989b55bb137b89068
Kelli Libin <km.putnam2@gmail.com>
2023-07-05 11:31:54 -0600

    created order schema

---
+++ models/Order.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+
+//validates product categories
+const SingleCartItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  product: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+});
+
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cartItems: [SingleCartItemSchema],
+    status: {
+      type: String,
+      enum: ["pending", "failed", "paid", "delivered", "cancelled"],
+      default: "pending",
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    //what we get back from Stripe to frontend
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    //once success, frontend sends us back payment id
+    paymentIntentId: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", OrderSchema);
