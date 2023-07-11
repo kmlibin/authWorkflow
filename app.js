9d530578413ca30f7d17b974bf8a8fd09604ed33
Kelli Libin <km.putnam2@gmail.com>
2023-07-11 10:10:46 -0600

    security packages

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const app = express();
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
 
 //database
 const connectDB = require("./db/connect");
@@ -27,6 +32,18 @@ const orderRouter = require("./routes/orderRoutes");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use(morgan("tiny"));
 app.use(express.json());
 //signing our cookies, now available in req.signedCookies
@@ -34,6 +51,7 @@ app.use(cookieParser(process.env.JWT_SECRET));
 //make public folder available (setting it up as our static assets);
 app.use(express.static("./public"));
 app.use(fileUpload());
+
 //home route
 app.get("/", (req, res) => {
   res.send("ecommerce api");
