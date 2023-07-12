a8d389ed3d453cc79416d22f8a16a73c7a9358e9
Kelli Libin <km.putnam2@gmail.com>
2023-07-12 12:31:19 -0600

    add post route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,10 +5,12 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  verifyEmail,
 } = require("../controllers/authController");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
+router.post("/verify-email", verifyEmail);
 
 module.exports = router;
