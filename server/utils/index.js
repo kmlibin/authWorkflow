615216c194f5f9cfe35479addadfcd2f0aa8aee3
Kelli Libin <km.putnam2@gmail.com>
2023-07-13 11:22:29 -0600

    added send reset email

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,12 +1,14 @@
 const { isTokenValid, createJWT, attachCookiesToResponse } = require("./jwt");
-const createTokenUser = require('./createTokenUser');
-const checkPermissions = require('./checkPermissions');
-const sendVerificationEmail = require('./sendVerificationEmail');
+const createTokenUser = require("./createTokenUser");
+const checkPermissions = require("./checkPermissions");
+const sendVerificationEmail = require("./sendVerificationEmail");
+const sendResetPasswordEmail = require("./sendResetPasswordEmail");
 module.exports = {
   createJWT,
   isTokenValid,
   attachCookiesToResponse,
   createTokenUser,
   checkPermissions,
-  sendVerificationEmail
+  sendVerificationEmail,
+  sendResetPasswordEmail,
 };
