05f75051bca28f29d080ca317b6d649c13eea040
Kelli Libin <km.putnam2@gmail.com>
2023-07-13 10:47:04 -0600

    check for refresh token, add it to payload

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,15 +1,36 @@
 const CustomError = require("../errors");
 const { isTokenValid } = require("../utils");
+const attachCookiesToResponse = require("../utils");
+const Token = require("../models/Token");
 
-const authenticateUser = (req, res, next) => {
-  const {refreshToken, accessToken} = req.signedCookies;
+const authenticateUser = async (req, res, next) => {
+  const { refreshToken, accessToken } = req.signedCookies;
   //check access token first b/c shorter expiration. if it's there, fine, just pass on to next req
   try {
-    if(accessToken){
-      const payload = isTokenValid(accessToken)
-      req.user = payload.user
-      return next()
+    if (accessToken) {
+      const payload = isTokenValid(accessToken);
+      req.user = payload.user;
+      return next();
     }
+    //if no access token
+    const payload = isTokenValid(refreshToken);
+    //check whether it exists and isValid is true
+    //when we set up refresh token, we set up a user and refresh token as the payload, which is why we can access here.
+    const existingToken = await Token.findOne({
+      user: payload.user.userId,
+      refreshToken: payload.refreshToken,
+    });
+    if (!existingToken || !existingToken?.isValid) {
+      throw new CustomError.UnauthenticatedError("Authentication Invalid");
+    }
+    //if valid, we want to pass it on and attach the cookies
+    attachCookiesToResponse({
+      res,
+      user: payload.user,
+      refreshToken: existingToken.refreshToken,
+    });
+    req.user = payload.user;
+    next();
   } catch (error) {
     throw new CustomError.UnauthenticatedError("Authentication Invalid");
   }
